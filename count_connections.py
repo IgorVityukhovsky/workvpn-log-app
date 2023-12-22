6c7d21ac7243348d83f444a52a4c98983a24dc27
IgorVityukhovsky <Relixinus@mail.ru>
2023-12-22 22:54:17 +0500

    modified:   app.yml

--- count_connections.py
+++ count_connections.py
@@ -3,3 +3,5 @@ import time
 while True:
     print("Hello world!")
     time.sleep(1)
+
+# k delete po app --force --grace-period 0 && k apply -f https://raw.githubusercontent.com/IgorVityukhovsky/workvpn-log-app/main/app.yml
